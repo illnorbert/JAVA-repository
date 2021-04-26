fe35f1472e0173edbbf9dd755fbed8d22005fab0
illnorbert <71004522+illnorbert@users.noreply.github.com>
2021-04-26 14:18:19 +0200

    feltöltés

--- amerikaiFoci.java
+++ amerikaiFoci.java
@@ -8,45 +8,88 @@ public static ArrayList<NFL> Jatekos= new ArrayList<>();
    
     
     public static void beolvasas(){
-        try{
-            RandomAccessFile file = new RandomAccessFile("NFL_iranyitok.txt","r");
+      try {
+            RandomAccessFile fajl = new RandomAccessFile("NFL_iranyitok.txt", "r");
+            String elsoSor = fajl.readLine();
+            if (elsoSor.contains(";")) {
+                Jatekos.add(new NFL(elsoSor));
+            }else{
+                System.out.println(elsoSor);}
             
-            while(file.getFilePointer()<file.length()){
-            Jatekos.add(new NFL(file.readLine())); 
-        }
-            file.close();
-            System.out.println("\nBeolvasva "+Jatekos.size());
-        }
-        catch(IOException e){
-            System.out.println("e");
+            while (fajl.getFilePointer() < fajl.length()) {
+                Jatekos.add(new NFL(fajl.readLine()));
+            }
+            fajl.close();
+            System.out.println("\nBeolvasva " + Jatekos.size() + " rekord.");
+        } catch (IOException e) {
+            System.out.println("Hiba történt!");
         }
     }
-
+    public static void feladat(int feladatszam){
+        System.out.println("\n" + feladatszam + ". feladat");}
+    
+    
+    
     public static void main(String[] args) {
         beolvasas();
+        feladat(1);
+        feladat(7);
+        System.out.println("Legjobb irányítók");
+        for (NFL i: Jatekos) {
+            if (i.iranyitomutato>=100 && i.yardMeterben(i.yard)>=4000) {
+                System.out.println("\t"+i.FormazottNev(i.nev));
+            }
+        }
+        
+        for (int i = 0; i < Jatekos.size(); i++) {
+            System.out.println(Jatekos.get(i).iranyitomutato);
+        }
+        
     }
-    
+
 }
-class NFL{
+
+class NFL {
+
     public String nev;
-    public int yardok;
-    public int kiserletek;
+    public int yard;
+    public int kiserlet;
     public int sikeres;
-    public int tdPasszok;
+    public int passz;
     public int eladott;
     public double iranyitomutato;
     public String egyetem;
-    
-    public NFL(String sor){
-        String [] adatok=sor.split(";");
+
+    public NFL(String sor) {
+        String[] adatok = sor.split(";");
         System.out.println(sor);
-        this.nev=adatok[0];
-        this.yardok=Integer.parseInt(adatok[1]);
-        this.kiserletek=Integer.parseInt(adatok[2]);
-        this.sikeres=Integer.parseInt(adatok[3]);
-        this.tdPasszok=Integer.parseInt(adatok[4]);
-        this.eladott=Integer.parseInt(adatok[5]);
-        this.iranyitomutato=Double.parseDouble(adatok[6]);
-        this.egyetem=adatok[7];
+        this.nev = adatok[0];
+        this.yard = Integer.parseInt(adatok[1]);
+        this.kiserlet = Integer.parseInt(adatok[2]);
+        this.sikeres = Integer.parseInt(adatok[3]);
+        this.passz = Integer.parseInt(adatok[4]);
+        this.eladott = Integer.parseInt(adatok[5]);
+        String replaceString = adatok[6].replace(",",".");
+        this.iranyitomutato = Double.parseDouble(replaceString);
+        this.egyetem = adatok[7];
+    }
+    /*private double Konvertal(String iranyitoMutato)
+    {
+        char decimalSeparator = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+        iranyitoMutato = iranyitoMutato.Replace(",", decimalSeparator).Replace(".", decimalSeparator);
+        if (double.TryParse(iranyitoMutato, out var ertek))
+            return ertek;
+        throw new FormatException("Hibás érték (irányítómutató)");
+    }*/
+	
+	public String FormazottNev(String nev)
+	{
+		String[] n = nev.split(" ");
+		n[n.length - 1] = n[n.length - 1].toUpperCase();
+		return String.join(" ", n);
+	}
+
+    int yardMeterben(int yard) {
+        return yard=44556;
     }
 }
